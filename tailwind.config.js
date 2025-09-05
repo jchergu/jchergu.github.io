dbbc9c288f6dfaa4499f0987d197ca72a9ec5cbc
Jacopo Chergui <jackchergui09@gmail.com>
2025-09-05 16:47:11 +0200

    add: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html",
-    "./assets/js/**/*.js"
+      "./*.html",
+      "./src/**/*.html",
+      "./public/**/*.html",
+      "./assets/js/**/*.js",
+      "./public/js/**/*.js"
   ],
   darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#fdb101ff",
+          orange: "#eb8317",
+          dark: "#1a1a1a",
+          rose: "#FF6666",
+          blue: "#BCE7FD",
+          white: "#f5f5dc",
+          purple: "#5D2E8C",
+          red: "#5E0B15"
+        }
+      }
+    }
   },
   plugins: [],
 }
-
